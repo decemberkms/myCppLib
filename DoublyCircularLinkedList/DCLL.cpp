fddf51fc84da87550f99751b01e4923dfa25b367
decemberkms <decemberkms@gmail.com>
2022-03-17 20:34:14 +0100

    Add doubly circular linked list

---
+++ DoublyCircularLinkedList/DCLL.cpp
@@ -0,0 +1,1 @@
+#include "DCLL.hpp"
---
+++ DoublyCircularLinkedList/DCLL.hpp
@@ -0,0 +1,14 @@
+#ifndef _DCLL_H_
+#define _DCLL_H_
+
+#include "Node.hpp"
+#include <iostream>
+
+class DCLL{
+private:
+    Node* head;
+public:
+
+};
+
+#endif
