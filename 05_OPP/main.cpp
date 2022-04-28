fec0544fcf47be7d49f5083e1d5b47f8c6bb784c
decemberkms <decemberkms@gmail.com>
2022-04-28 20:57:16 +0200

    Add pointer object

--- 05_OPP/main.cpp
+++ 05_OPP/main.cpp
@@ -7,9 +7,12 @@ int main(){
     Account min_accout("Min", 2000.0); 
     Account pee_account("Pee", 300.0);
     Account default_account;
-    
+    Account *pointer_accout = new Account("Pointer", 1000.0);
+
     min_accout.printInfo();
     pee_account.printInfo();
     default_account.printInfo();
+    pointer_accout->printInfo();
+    delete pointer_accout;
     return 0;
 }
